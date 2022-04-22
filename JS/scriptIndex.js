c965a82f36ea1300e8ee19bb6292c704823f36ae
Sunrise9871 <user@company.com>
2022-04-22 15:25:18 +0700

    testCommit

---
+++ JS/scriptIndex.js
@@ -0,0 +1,12 @@
+
+
+$(function() {
+	//$('.Input_login').attr('value');
+	
+	var text = document.getElementsByTagName("input")[0];
+	//alert("pizdec");
+	
+	document.querySelector("#loginButton").onclick = function(){
+		//document.getElementById('login').value = "pizdec 2";
+	}
+});
