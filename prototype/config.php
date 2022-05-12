7b68b7584c9c45ae9cd77536ea3263e6886096e2
opuperon <65608610+opuperon@users.noreply.github.com>
2022-05-12 17:52:23 +0700

    prototype1

--- prototype/config.php
+++ prototype/config.php
@@ -1,8 +1,8 @@
 <?php
-    define('USER', 'root');
-    define('PASSWORD', 'root');
-    define('HOST', 'localhost');
-    define('DATABASE', 'todolits');
+    define('USER', 'todolist');
+    define('PASSWORD', 'todolist');
+    define('HOST', '26.161.34.238');
+    define('DATABASE', 'todolist');
     try {
         $connection = new PDO("mysql:host=".HOST.";dbname=".DATABASE, USER, PASSWORD);
     } catch (PDOException $e) {
